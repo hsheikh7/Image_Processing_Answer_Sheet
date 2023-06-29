d2df31d5e96991520a2206f9dc8833e68d11a430
hsheikh7 <hassan.sheikh85@gmail.com>
2023-06-29 21:38:56 +0330

    Added static method part 5

--- answer_sheet_correction.py
+++ answer_sheet_correction.py
@@ -7,14 +7,17 @@ from model import ClassScore
 path = 'images/image0000013A.tif'
 path_to_key = 'images/key.png'
 path_to_key_csv = 'images/key.csv'
+path_folder = 'images'
 
 test1 = ClassScore(path,path_to_key ) 
 # print(test1.score())
 # test1.representation()
 # test1.save_status("outputfile.csv")
-test1.save_allstatus("outputfile2.csv")
 
-ClassScore.save_allstatus()
+# ClassScore.save_allstatus(path, path_to_key, "outputfile2.csv")
+
+ClassScore.save_all(path_folder, path_to_key, "output_save_all.csv")
+
 
 #----------------- Presentation  -----------------
 
--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from functions import generate_key, detect_answers, find_the_answer_datasheet, find_the_question, evaluation, presentation
 from functions import add_answers_to_datasheet, detect_questions_in_datasheet, presentation, add_checked_to_datasheet
+import os
 
 class ClassScore:
     def __init__(self, answer_sheet_path, answer_key_path):
@@ -55,16 +56,53 @@ class ClassScore:
         df['checked'] = df['checked'].fillna('-')
 
         df.to_csv(output_file, index=False)
-
-    
-    @staticmethod
-    def save_allstatus(answer_sheets, answer_keys, output_file):
-        # Save the status of all answer sheets in a CSV file
-        
         pass 
     
     @staticmethod
-    def save_all(answer_sheets_folder, answer_keys_folder, output_file):
+    def save_allstatus(answer_sheet_path, answer_key_path, output_file):
+        key_df = generate_key(answer_key_path)
+
+        answer_sheet_df = detect_answers(answer_sheet_path)
+        answer_sheet_df = add_answers_to_datasheet(answer_sheet_df)
+        answer_sheet_df = detect_questions_in_datasheet(answer_sheet_df)
+        answer_sheet_df = add_checked_to_datasheet(key_df, answer_sheet_df)
+
+        answer_sheet_df = answer_sheet_df.drop(['x', 'y', 'x2', 'y2'], axis=1)
+        answer_sheet_df = answer_sheet_df.sort_values('question')
+
+        df = pd.DataFrame()
+        df['question'] = range(1,166)
+        df['checked'] = "-"
+
+        merged_df = df.merge(answer_sheet_df, on='question', how='left')
+        df['checked'] = merged_df['checked_y']
+        df['checked'] = df['checked'].fillna('-')
+
+        df['2'] = os.path.basename(answer_sheet_path)
+        df.insert(0, 'file.name', df['2'])
+        df = df.drop('2', axis=1)
+
+        df.to_csv(output_file, index=False)
+                
+        pass 
+        
+        
+    
+    @staticmethod
+    def save_all(answer_sheets_folder, key_path, output_file):
         # Read all answer sheets and keys, calculate scores, and save them in a CSV file
         
+
+        # Calculate the score and return a float value between 0 and 100
+        key_df = generate_key(answer_key_path)
+
+        answer_sheet_df = detect_answers(answer_sheet_path)
+        answer_sheet_df = add_answers_to_datasheet(answer_sheet_df)
+        answer_sheet_df = detect_questions_in_datasheet(answer_sheet_df)
+        answer_sheet_df = add_checked_to_datasheet(key_df, answer_sheet_df)
+        
+        score = evaluation(answer_sheet_df)
+
+
+
         pass
